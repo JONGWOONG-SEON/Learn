e582aeb8f62aa80d02b2a7c751aa7f11e30bf64d
JONGWOONG-SEON <www11288@naver.com>
2025-02-04 00:07:46 +0900

    ADD:DesignPattern Builder

---
+++ Pattern/DesignPattern/CreationalPattern/Builder/Builder.py
@@ -0,0 +1,94 @@
+import sys
+
+class Product:
+    def __init__(self):
+        self._partA = ""
+        self._partB = ""
+        self._partC = ""
+
+    def makeA(self, part):
+        self._partA = part
+
+    def makeB(self, part):
+        self._partB = part
+    
+    def makeC(self, part):
+        self._partC = part
+
+    def get(self):
+        return self._partA + " " + self._partB + " " + self._partC
+
+class Builder:
+    def __init__(self):
+        self._product = Product()
+    
+    def get(self):
+        return self._product
+    
+    def buildPartA():
+        pass
+    
+    def buildPartB():
+        pass
+
+    def buildPartC():
+        pass
+
+class ContreateBuilderX(Builder):
+    def __init__(self):
+        Builder.__init__(self)
+        
+    def buildPartA(self):
+        self._product.makeA("A-X")
+    
+    def buildPartB(self):
+        self._product.makeB("B-X")
+
+    def buildPartC(self):
+        self._product.makeC("C-X")
+
+class ContreateBuilderY(Builder):
+    def __init__(self):
+        Builder.__init__(self)
+        
+    def buildPartA(self):
+        self._product.makeA("A-Y")
+    
+    def buildPartB(self):
+        self._product.makeB("B-Y")
+
+    def buildPartC(self):
+        self._product.makeC("C-Y")
+
+
+class Director:
+    def __init__(self):
+        self._builder = None
+
+    def set(self, builder):
+        self._builder = builder
+    
+    def get(self):
+        return self._builder.get()
+    
+    def construct(self):
+        self._builder.buildPartA()
+        self._builder.buildPartB()
+        self._builder.buildPartC()
+
+if __name__ == "__main__":
+    builderX = ContreateBuilderX()
+    builderY = ContreateBuilderY()
+
+    director = Director()
+    director.set(builderX)
+    director.construct()
+
+    product1 = director.get()
+    print("1st product parts: " + product1.get())
+  
+    director.set(builderY)  
+    director.construct()
+    
+    product2 = director.get()
+    print("2nd product parts: " + product2.get())
